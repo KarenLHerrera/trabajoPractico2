f5be5d0c680d4e39ac3da427fc243aed177ff4b3
KarenHerreraDev <karenherrera.dev@gmail.com>
2025-05-03 10:47:40 -0300

    Tp Final

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
       <nav>
         <a href="">Home</a>
         <a href="nosotros.html" target="_blank">Nosotros</a>
-        <a href="https://frt.utn.edu.ar" target="_blank">Facultad Regional Tucuman - UTN</a>
+        <a href="https://frt.utn.edu.ar/" target="_blank">Facultad Regional Tucuman - UTN</a>
       </nav>
     </div>
   );
