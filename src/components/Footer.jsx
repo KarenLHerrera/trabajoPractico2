e0e351c38ab4bf09204eb7c663e7c0bed6613fb4
KarenHerreraDev <karenherrera.dev@gmail.com>
2025-05-03 10:45:42 -0300

    tp final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import "../css/Footer.css"
 
 const Footer = () => {
   return (
-    <div >
-      <h3 className='Footer'>Universidad Tecnologica Nacional - Facultad Regional Tucuman</h3>
+    <div className='Footer' >
+      <h3 className='m-3' >Universidad Tecnologica Nacional - Facultad Regional Tucuman</h3>
       <Map/>
     </div>
   )
